e34f92bfcaa399524862ae9f7fde9a30df684894
hemadri <hemadhrikantam@gmail.com>
2022-07-28 15:03:49 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+import 'login_page.dart';
+import 'registration.dart';
+// import 'loginpage.dart';
+
+void main()  async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+DatabaseReference dbRef=FirebaseDatabase.instance.reference().child("users");
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
